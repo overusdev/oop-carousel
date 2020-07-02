3f6804e93f07443d706b9e2fed9add076bb819e0
Сергей Вилков <overus.dev@gmail.com>
2020-07-02 13:20:27 +0300

    Wrapped

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -2,14 +2,26 @@ export class CarouselFrame {
     constructor(settings) {
         this._settings = settings;
         this._element = document.querySelector(this._settings.element);
-        this._slides = this._element.children;
+        this._slides = [].slice.call(this._element.children);
+        this._slidesWrapper = document.createElement('div');
+        this._slidesInner = document.createElement('div');
+    }
+
+    addWrapper() {
+        this._slidesWrapper.className = 'carousel';
+        this._slidesInner.className = 'carousel__inner';
+        this._element.appendChild(this._slidesWrapper);
     }
 
     slidesStyles() {
         const slides = this._slides;
-        Object.keys(slides).forEach(index => {
-            const elements = slides[index];
-            elements.style.width = '100%';
+        const wrapper = this._slidesWrapper;
+        const inner = this._slidesInner;
+
+        slides.forEach(function (item) {
+            wrapper.appendChild(inner);
+            inner.appendChild(item);
+            item.classList.add('carousel__img');
         });
     }
 
@@ -17,8 +29,10 @@ export class CarouselFrame {
 
 export class Carousel extends CarouselFrame {
     init() {
-        console.log(this);
+        // console.log(this);
+        this.addWrapper();
         this.slidesStyles();
+
     }
 }
 
